d1a2758572e1b70fa396e2d9c8f667b8113a7415
Y3tiq <oleg_podryadov@mail.ru>
2019-04-02 15:38:54 +0500

    change port
    change port

--- contrib/bitrpc/bitrpc.py
+++ contrib/bitrpc/bitrpc.py
@@ -11,9 +11,9 @@ rpcpass = ""
 
 
 if rpcpass == "":
-    access = ServiceProxy("http://127.0.0.1:28933")
+    access = ServiceProxy("http://127.0.0.1:7632")
 else:
-    access = ServiceProxy("http://"+rpcuser+":"+rpcpass+"@127.0.0.1:28933")
+    access = ServiceProxy("http://"+rpcuser+":"+rpcpass+"@127.0.0.1:7632")
 cmd = sys.argv[1].lower()
 
 if cmd == "backupwallet":
