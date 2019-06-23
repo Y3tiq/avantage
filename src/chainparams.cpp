0d919a2b6072afa8d3cbf5f9e323523f158c8329
Y3tiq <oleg_podryadov@mail.ru>
2019-06-23 15:06:23 +0500

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -146,8 +146,8 @@ public:
     	if (true) {
         	printf("Genesis mining started\n");
         	genesis.nNonce = 0;
-        	hashGenesisBlock = uint256S("0x001");
-        	for(genesis.nNonce = 0; uint256S(genesis.GetHash()) > uint256S("0000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"); genesis.nNonce++){  }
+        	hashGenesisBlock = uint256("0x001");
+        	for(genesis.nNonce = 0; uint256(genesis.GetHash()) > uint256("0000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"); genesis.nNonce++){  }
         	printf("New genesis merkle root: %s\n", genesis.hashMerkleRoot.ToString().c_str());
         	printf("New genesis nonce: %lu\n", (unsigned long)genesis.nNonce);
         	printf("New genesis hash: %s\n", genesis.GetHash().ToString().c_str());
